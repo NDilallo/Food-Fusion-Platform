d868887dc55a8221a1048e0b67078729017298e0
Nick Dilallo <nickd5465@gmail.com>
2024-04-09 19:53:44 -0500

    Spring Boot Initialization & Dependencies

---
+++ src/test/java/com/FoodFusion/FoodFusionPlatform/FoodFusionPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FoodFusion.FoodFusionPlatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodFusionPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
