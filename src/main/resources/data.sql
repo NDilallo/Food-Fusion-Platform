8cf8f76387bf27e1310daa6a6cae538c7f8b3810
Fritz Nastor <98058294+fritznastor@users.noreply.github.com>
2024-04-30 15:49:43 -0500

    Fritz setup

--- src/main/resources/data.sql
+++
@@ -1,7 +0,0 @@
--- File for adding default sql data
-
--- Testing Data
-insert into test(dept, num) values ('test1', '100');
-insert into test(dept, num) values ('test2', '101');
-
-insert into app_user(username, pass) values ('Nick.Dilallo', 'abc123');
