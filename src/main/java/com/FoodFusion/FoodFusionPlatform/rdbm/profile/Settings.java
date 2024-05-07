7b32ae28c13db34a80601bbb99072547bc6bc2e7
Fritz Nastor <98058294+fritznastor@users.noreply.github.com>
2024-05-06 20:02:14 -0500

    fixed package name

--- src/main/java/com/FoodFusion/FoodFusionPlatform/rdbm/profile/Settings.java
+++ src/main/java/com/FoodFusion/FoodFusionPlatform/rdbm/profile/Settings.java
@@ -1,4 +1,4 @@
-package com.FoodFusion.FoodFusionPlatform.rdbm.Profile;
+package com.FoodFusion.FoodFusionPlatform.rdbm.profile;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
