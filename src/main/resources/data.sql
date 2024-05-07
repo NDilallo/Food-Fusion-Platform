ce510a0c55ea338d874be3b9eb7a559955d8276d
Nick Dilallo <nickd5465@gmail.com>
2024-05-06 20:34:07 -0500

    split into seperate profiles for dev. and prod.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,6 +2,6 @@
 
 -- Testing Data
 insert into test(dept, num) values ('test1', '100');
-insert into test(dept, num) values ('test2', '101');
+-- insert into test(dept, num) values ('test2', '101');
 
-insert into app_user(username, pass) values ('Nick.Dilallo', 'abc123');
+-- insert into app_user(username, pass) values ('Nick.Dilallo', 'abc123');
