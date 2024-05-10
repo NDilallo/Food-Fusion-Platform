3fc74308783574945edfbcdf8be6c34d7da7c0c8
Nick Dilallo <nickd5465@gmail.com>
2024-05-10 12:35:27 -0500

    package issue

--- src/main/java/com/FoodFusion/FoodFusionPlatform/rdbm/Upload/Draft.java
+++ src/main/java/com/FoodFusion/FoodFusionPlatform/rdbm/Upload/Draft.java
@@ -1,4 +1,4 @@
-package com.FoodFusion.FoodFusionPlatform.rdbm.Upload;
+package com.FoodFusion.FoodFusionPlatform.rdbm.upload;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
