14489fd78c143f14ec666a473456670bfc12e7fd
Marisa Ban <91231395+marisa-ban@users.noreply.github.com>
2024-05-14 16:39:38 -0500

    Create PostedRecipeService.java

---
+++ src/main/java/com/FoodFusion/FoodFusionPlatform/services/PostedRecipeService.java
@@ -0,0 +1,40 @@
+package com.FoodFusion.FoodFusionPlatform.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.FoodFusion.FoodFusionPlatform.rdbm.profile.PostedRecipe;
+import com.FoodFusion.FoodFusionPlatform.rdbm.profile.PostedRecipeRepository;
+
+import lombok.extern.log4j.Log4j2;
+
+@Service
+@Log4j2
+public class PostedRecipeService {
+    @Autowired
+    private PostedRecipeRepository repo;
+
+    public List<PostedRecipe> list() {
+        log.traceEntry("Enter List");
+        List<PostedRecipe> retval = StreamSupport.stream(repo.findAll().spliterator(), false).collect(Collectors.toList());
+        log.traceExit("Exit list", retval);
+        return retval;
+    }
+
+    public PostedRecipe save(PostedRecipe postedRecipe) {
+        log.traceEntry("enter save", postedRecipe);
+        repo.save(postedRecipe);
+        log.traceExit("Exit list", postedRecipe);
+        return postedRecipe;                
+    }
+
+    public void delete(long id) {
+        log.traceEntry("Enter delete", id);
+        repo.deleteById(id);
+        log.traceExit("Exit delete");
+    }
+}
