6eeb06161d631c6db2511b6fb9e632ff02994115
Nick Dilallo <nickd5465@gmail.com>
2024-05-13 21:39:00 -0500

    linked User table to Profile table

--- src/main/java/com/FoodFusion/FoodFusionPlatform/rdbm/users/User.java
+++ src/main/java/com/FoodFusion/FoodFusionPlatform/rdbm/users/User.java
@@ -1,5 +1,7 @@
 package com.FoodFusion.FoodFusionPlatform.rdbm.users;
 
+import com.FoodFusion.FoodFusionPlatform.rdbm.profile.Profile;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -28,9 +30,8 @@ public class User {
     @Size(min= 3,max = 15, message = "password must be between 3 and 15 characters")
     private String pass;
 
-    /*
     @OneToOne
     @JoinColumn(name = "profile_id") // profile_id is the name of col in this table w/ the foregin key
     private Profile profile_link; // profile_link will link to the primary key of the Independent entity 
-    */
+    
 }
