bb477a2d7d629658a71cdf21673a37e18eddc622
Marisa Ban <91231395+marisa-ban@users.noreply.github.com>
2024-05-14 19:40:46 -0500

    Add log stuff
    may or may not

--- src/main/java/com/FoodFusion/FoodFusionPlatform/rdbm/homePage/RestaurantRepository.java
+++ src/main/java/com/FoodFusion/FoodFusionPlatform/rdbm/homePage/RestaurantRepository.java
@@ -1,7 +1,11 @@
 package com.FoodFusion.FoodFusionPlatform.rdbm.homePage;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface RestaurantRepository extends CrudRepository<Restaurant, Long>{
     public Restaurant findByName(String name);
+
+    public List<Restaurant> findByCuisine(String cuisine);
 }
--- src/main/java/com/FoodFusion/FoodFusionPlatform/rdbm/profile/PostedRecipeRepository.java
+++ src/main/java/com/FoodFusion/FoodFusionPlatform/rdbm/profile/PostedRecipeRepository.java
@@ -1,7 +1,11 @@
 package com.FoodFusion.FoodFusionPlatform.rdbm.profile;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface PostedRecipeRepository extends CrudRepository<PostedRecipe, Long> {
     public PostedRecipe findByName(String name);
+
+    public List<PostedRecipe> findByRecipeCuisine(String recipeCuisine);
 }
