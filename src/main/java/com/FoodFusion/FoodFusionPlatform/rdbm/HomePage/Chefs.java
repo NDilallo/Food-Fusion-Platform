b97f9751095d1d64822a5248d991ba97307aa492
Dsolanki658 <sdhruvi658@gmail.com>
2024-05-17 11:54:33 -0500

    added chefs.java file

---
+++ src/main/java/com/FoodFusion/FoodFusionPlatform/rdbm/HomePage/Chefs.java
@@ -0,0 +1,13 @@
+package com.FoodFusion.FoodFusionPlatform.rdbm.homePage;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Chefs {
+    private String name;
+    private String specialtyofchef;
+}
