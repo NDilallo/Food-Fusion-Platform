958ce07cd8979ba3f200ad413a8d8563bb80e0c8
Marisa Ban <91231395+marisa-ban@users.noreply.github.com>
2024-05-20 11:58:33 -0500

    Create Restaurant rest controller

---
+++ src/main/java/com/FoodFusion/FoodFusionPlatform/controller/profile/RestaurantRestController.java
@@ -0,0 +1,88 @@
+package com.FoodFusion.FoodFusionPlatform.controller.profile;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.FoodFusion.FoodFusionPlatform.rdbm.homePage.Restaurant;
+import com.FoodFusion.FoodFusionPlatform.services.RestaurantService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.extern.log4j.Log4j2;
+
+/*
+ * Documented controller using OpenAPI
+ */
+@RestController
+@RequestMapping("/api/rating")
+@Tag(name = "Rating", description = "All ratings")
+@Log4j2
+public class RestaurantRestController {
+    @Autowired
+    private RestaurantService service;
+
+    @GetMapping
+    @Operation(summary = "Returns all the restaurants in the system")
+    @ApiResponse(responseCode = "200", description = "valid response", 
+        content = {@Content(mediaType="application/json", schema=@Schema(implementation=Restaurant.class))})
+    public List<Restaurant> list() {
+        return service.list();
+    }
+
+    @PostMapping
+    @Operation(summary = "Save the restaurant and returns the saved restaurant's id")
+    public long save(@RequestBody Restaurant r) {
+        log.traceEntry("enter save", r);
+        service.save(r);
+        log.traceExit("exit save", r);        
+        return r.getRestaurantId();
+    }
+
+    @PostMapping("/validated")
+    @Operation(summary = "Save the restaurant")
+    public ResponseEntity<String> validatedSave(@Valid @RequestBody Restaurant r) {
+        log.traceEntry("enter save", r);
+        service.save(r);
+        log.traceExit("exit save", r);
+        return ResponseEntity.ok("new id is " + r.getRestaurantId());
+    }
+
+    @DeleteMapping
+    @Operation(summary = "Delete the rating")
+    public void delete(long id) {
+        log.traceEntry("Enter delete", id);
+        service.delete(id);
+        log.traceExit("Exit delete");
+    }
+    
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return errors;
+    }
+}
