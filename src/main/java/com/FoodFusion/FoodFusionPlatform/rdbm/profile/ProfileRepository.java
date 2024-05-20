daba8663f0fd09f6e66d65043ea06e914acf0dc3
Fritz Nastor <98058294+fritznastor@users.noreply.github.com>
2024-05-20 14:04:09 -0500

    profile repo cleaned up

--- src/main/java/com/FoodFusion/FoodFusionPlatform/rdbm/profile/ProfileRepository.java
+++ src/main/java/com/FoodFusion/FoodFusionPlatform/rdbm/profile/ProfileRepository.java
@@ -2,11 +2,9 @@ package com.FoodFusion.FoodFusionPlatform.rdbm.profile;
 
 import org.springframework.data.repository.CrudRepository;
 
-
 /**
  * @see JpaRepository 
  */
-
  public interface ProfileRepository extends CrudRepository<Profile, Long> {
 
  }
