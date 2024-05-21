cd0673de4046af6f6057bfc72cf3caba527b7de6
mattnice <mattnice02@gmail.com>
2024-05-20 19:04:02 -0500

    fixed issues with comments and stuff

--- src/main/java/com/FoodFusion/FoodFusionPlatform/rdbm/upload/Draft.java
+++ src/main/java/com/FoodFusion/FoodFusionPlatform/rdbm/upload/Draft.java
@@ -4,9 +4,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-
+import jakarta.validation.constraints.NotNull;
+import com.FoodFusion.FoodFusionPlatform.rdbm.profile.Profile;
 import lombok.Data;
 
 @Data
@@ -31,4 +34,9 @@ public class Draft {
     @NotBlank(message = "Cuisine must be set")
     @Size(min = 2, message = "Cuisine must be 2 or more characters")
     private String cuisine;
+
+   // @NotNull(message = "User ID must be set")
+   // @ManyToOne
+    //@JoinColumn(name = "user_id", referencedColumnName = "userID", nullable = false)
+    //private Profile user; // Foreign key to Profile entity
 }
