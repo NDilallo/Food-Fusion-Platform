6bbab01f38fc1714680719cadffe913a344f62d6
Nick Dilallo <nickd5465@gmail.com>
2024-05-30 20:35:33 -0500

    started adding axios api call

--- src/main/react-web/material-dashboard-material-ui-v4-main/src/components/Test/ListingTest.jsx
+++ src/main/react-web/material-dashboard-material-ui-v4-main/src/components/Test/ListingTest.jsx
@@ -1,36 +1,13 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { useEffect } from 'react';
 
 const ListingTest = () => {
-    const dummyData = [
-        {
-            "id": 1,
-            "username": "n_dilallo",
-            "pass": "abc123",
-            "profile_link": {
-                "userID": 1,
-                "postedRecipesTableID": 0,
-                "settingsTableID": 0,
-                "followingTableID": 0,
-                "draftsTableID": 0,
-                "commentID": 0,
-                "notificationID": 0
-            }
-        },
-        {
-            "id": 0,
-            "username": "new_user",
-            "pass": "user_password",
-            "profile_link": {
-                "userID": 2,
-                "postedRecipesTableID": 1,
-                "settingsTableID": 1,
-                "followingTableID": 1,
-                "draftsTableID": 1,
-                "commentID": 1,
-                "notificationID": 1
-            }
-        }
-    ];
+
+    const [users, setUsers] = useState([])
+
+    useEffect(() => {
+        
+    })
 
     return (
         <div>
