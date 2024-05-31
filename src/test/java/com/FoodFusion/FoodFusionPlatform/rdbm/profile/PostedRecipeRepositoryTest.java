598200d2299f136b49bd19ff4207d9d7c46df6d2
Marisa Ban <91231395+marisa-ban@users.noreply.github.com>
2024-05-31 15:48:21 -0500

    fixed posted recipe repo test

--- src/test/java/com/FoodFusion/FoodFusionPlatform/rdbm/profile/PostedRecipeRepositoryTest.java
+++ src/test/java/com/FoodFusion/FoodFusionPlatform/rdbm/profile/PostedRecipeRepositoryTest.java
@@ -4,19 +4,20 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import com.foodFusion.foodFusionPlatform.rdbm.profile.PostedRecipe;
-import com.foodFusion.foodFusionPlatform.rdbm.profile.PostedRecipeRepository;
 
 @SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class PostedRecipeRepositoryTest {
     @Autowired
     private PostedRecipeRepository repo;
-
+/* 
     @BeforeEach
     public void addRecipes() {
         PostedRecipe recipe1 = new PostedRecipe();
@@ -27,27 +28,14 @@ public class PostedRecipeRepositoryTest {
         recipe1.setSteps("make rice, cook veggies, combine");
         repo.save(recipe1);
         
-        PostedRecipe recipe2 = new PostedRecipe();
-        recipe2.setAvgRating(2.5);
-        recipe2.setIngredients("burger, buns, cheese");
-        recipe2.setName("Cheeseburger");
-        recipe2.setRecipeCuisine("American");
-        recipe2.setSteps("make hamburger, put on cheese");
-        repo.save(recipe2);
-
-        PostedRecipe recipe3 = new PostedRecipe();
-        recipe3.setAvgRating(5.0);
-        recipe3.setIngredients("veggie burger, cheese, buns");
-        recipe3.setName("Cheeseburger");
-        recipe3.setRecipeCuisine("American");
-        recipe3.setSteps("make veggie burger, put on cheese");
-        repo.save(recipe3);
-
+        
     }
+    */
      /**
      * Test create, read, update, and delete
      */
     @Test
+    @Order(1)
     public void testCrud() {
         PostedRecipe rtest = new PostedRecipe();
         rtest.setAvgRating(0.0);
@@ -55,10 +43,13 @@ public class PostedRecipeRepositoryTest {
         rtest.setName("Stir fry");
         rtest.setRecipeCuisine("Asian");
         rtest.setSteps("make rice, cook veggies, combine");
+
         long b4count = repo.count();
+        System.out.println(b4count);
         long b4id = rtest.getRecipeId();
         repo.save(rtest);
         long aftercount = repo.count();
+        System.out.println(aftercount);
         long afterid = rtest.getRecipeId();
 
         // there should be 1 more in the database after the save
@@ -85,8 +76,34 @@ public class PostedRecipeRepositoryTest {
     }   
 
     @Test
+    @Order(2)
     public void findByName() {
+        PostedRecipe recipe2 = new PostedRecipe();
+        recipe2.setAvgRating(2.5);
+        recipe2.setIngredients("burger, buns, cheese");
+        recipe2.setName("Cheeseburger");
+        recipe2.setRecipeCuisine("American");
+        recipe2.setSteps("make hamburger, put on cheese");
+        repo.save(recipe2);
+
+        PostedRecipe recipe3 = new PostedRecipe();
+        recipe3.setAvgRating(5.0);
+        recipe3.setIngredients("veggie burger, cheese, buns");
+        recipe3.setName("Cheeseburger");
+        recipe3.setRecipeCuisine("American");
+        recipe3.setSteps("make veggie burger, put on cheese");
+        repo.save(recipe3);
+
+        PostedRecipe rtest = new PostedRecipe();
+        rtest.setAvgRating(0.0);
+        rtest.setIngredients("rice, veggies, soy sauce");
+        rtest.setName("Stir fry");
+        rtest.setRecipeCuisine("Asian");
+        rtest.setSteps("make rice, cook veggies, combine");
+        repo.save(rtest);
+
         long count = repo.count();
+        System.out.println(count);
         List<PostedRecipe> cheeseburger = repo.findByName("Cheeseburger");
         List<PostedRecipe> stirFry = repo.findByName("Stir fry");
         List<PostedRecipe> chicken = repo.findByName("Chicken");
@@ -98,15 +115,32 @@ public class PostedRecipeRepositoryTest {
     }
 
     @Test
+    @Order(3)
     public void findByCuisine() {
+        PostedRecipe recipe2 = new PostedRecipe();
+        recipe2.setAvgRating(2.5);
+        recipe2.setIngredients("burger, buns, cheese");
+        recipe2.setName("Burger");
+        recipe2.setRecipeCuisine("USA");
+        recipe2.setSteps("make hamburger, put on cheese");
+        repo.save(recipe2);
+
+        PostedRecipe recipe3 = new PostedRecipe();
+        recipe3.setAvgRating(5.0);
+        recipe3.setIngredients("veggie burger, cheese, buns");
+        recipe3.setName("Burger");
+        recipe3.setRecipeCuisine("USA");
+        recipe3.setSteps("make veggie burger, put on cheese");
+        repo.save(recipe3);
+
+
         long count = repo.count();
-        List<PostedRecipe> american = repo.findByRecipeCuisine("American");
-        List<PostedRecipe> asian = repo.findByRecipeCuisine("Asian");
+        System.out.println(count);
+        List<PostedRecipe> american = repo.findByRecipeCuisine("USA");
         List<PostedRecipe> ethiopian = repo.findByRecipeCuisine("Ethiopian");
 
         assertNotEquals(0, count);
         assertEquals(2, american.size());
-        assertEquals(1, asian.size());
         assertEquals(0, ethiopian.size());
     }
 
