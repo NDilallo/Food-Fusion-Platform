6e2cf3310ec0649178c44b7f7469323d3e91e87e
Marisa Ban <91231395+marisa-ban@users.noreply.github.com>
2024-06-06 22:35:06 -0500

    Update Upload.js
    add cuisine picker and store all values in respective vars

--- src/main/react-web/material-dashboard-material-ui-v4-main/src/views/Upload/Upload.js
+++ src/main/react-web/material-dashboard-material-ui-v4-main/src/views/Upload/Upload.js
@@ -1,25 +1,44 @@
-import React from "react";
-import { Box, TextField } from "@material-ui/core";
+import React, { useState } from "react";
+import { Box, TextField, MenuItem } from "@material-ui/core";
+
 
 const cuisines = [
     {
-      label: 'American',
+        value: 'American',
+        label: 'American',
     },
     {
-      label: 'Asian',
+        value: 'Asian',
+        label: 'Asian',
     },
     {
-      label: 'Mexican',
+        value: 'Mexican',
+        label: 'Mexican',
     },
     {
-      label: 'Ethiopian',
+        value: 'Ethiopian',
+        label: 'Ethiopian',
     },
     {
+        value: 'Other',
         label: 'Other',
-      },
+    },
   ];
 
 export default function UserPage() {
+    const [recipeName, setRecipeName] = useState('');
+    const [ingredients, setIngredients] = useState('');
+    const [description, setDescription] = useState('');
+    const [cuisine, setCuisine] = useState('');
+/*
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setPostData(prevState => ({
+            ...prevState,
+            [name]: value,
+        }));
+    };
+*/
     return (
       <Box
         component="form"
@@ -31,25 +50,61 @@ export default function UserPage() {
       >
         <h1>Upload a new recipe</h1>
         <div>
-            <TextField fullWidth id="outlined-basic" label="Recipe Name" variant="outlined" />
-        </div>
-        <div>
-            <TextField
-            fullWidth 
-            id="outlined-textarea"
-            label="Ingredients"
-            placeholder="Placeholder"
-            multiline
+            <TextField 
+                fullWidth 
+                id="outlined-basic" 
+                label="Recipe Name" 
+                variant="outlined" 
+                value={recipeName}
+                onChange={(e) => setRecipeName(e.target.value)}
             />
         </div>
         <div>
             <TextField
-            fullWidth 
-            id="outlined-textarea"
-            label="Directions"
-            placeholder="Placeholder"
-            multiline
+                fullWidth 
+                id="outlined-textarea"
+                label="Ingredients"
+                placeholder="Placeholder"
+                multiline
+                value={ingredients}
+                onChange={(e) => setIngredients(e.target.value)}
             />
+        </div>
+        <div>
+            <TextField
+                fullWidth 
+                id="outlined-textarea"
+                label="Directions"
+                placeholder="Placeholder"
+                multiline
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+            />
+        </div>
+        <div>
+            <TextField
+                id="outlined-select-cuisine"
+                select
+                label="Select"
+                helperText="Please select your cuisine"
+                value={cuisine}
+                onChange={(e) => setCuisine(e.target.value)}
+                >
+                {cuisines.map((option) => (
+                    <MenuItem key={option.value} value={option.value}>
+                        {option.label}
+                    </MenuItem>
+                ))}
+            </TextField>
+        </div>
+        <p>Current recipe name: {recipeName}</p>
+        <p>Current ingredients: {ingredients}</p>
+        <p>Current descrip: {description}</p>
+        <p>Current cuisine: {cuisine}</p>
+        
+        
+        <div>
+        
         </div>
       </Box>
     );
