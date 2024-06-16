1407c00d769ca5b60b0a200fa125047c8aaa0083
Marisa Ban <91231395+marisa-ban@users.noreply.github.com>
2024-06-15 20:08:04 -0500

    add bootsrap to upload.js

---
+++ src/main/react-web/material-dashboard-material-ui-v4-main/src/package-info.java
@@ -0,0 +1,7 @@
+/*
+ * Marisa Ban: Upload.js in views/Upload folder
+ * Marisa Ban non persistence extra: added boostrap elements to Upload.js 
+ * (ie Container class, margin/spacing classes, and button variants)
+ * 
+ * Marisa Ban and Fritz Nastor: Search.js in views/Search folder
+ */
