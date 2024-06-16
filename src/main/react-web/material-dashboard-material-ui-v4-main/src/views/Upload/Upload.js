4ee39c3cfde9a9f3cb20848cf3d23b1c378cf278
Marisa Ban <91231395+marisa-ban@users.noreply.github.com>
2024-06-16 12:59:19 -0500

    Add bootstrap to Upload.js

--- src/main/react-web/material-dashboard-material-ui-v4-main/src/views/Upload/Upload.js
+++ src/main/react-web/material-dashboard-material-ui-v4-main/src/views/Upload/Upload.js
@@ -2,172 +2,167 @@ import React, { useState } from "react";
 import { Box, TextField, MenuItem, Button } from "@material-ui/core";
 import axios from "axios";
 
-
 const cuisines = [
-   {
-       value: 'American',
-       label: 'American',
-   },
-   {
-       value: 'Asian',
-       label: 'Asian',
-   },
-   {
-       value: 'Mexican',
-       label: 'Mexican',
-   },
-   {
-       value: 'Ethiopian',
-       label: 'Ethiopian',
-   },
-   {
-       value: 'Other',
-       label: 'Other',
-   },
- ];
-
+  { value: "American", label: "American" },
+  { value: "Asian", label: "Asian" },
+  { value: "Mexican", label: "Mexican" },
+  { value: "Ethiopian", label: "Ethiopian" },
+  { value: "Other", label: "Other" },
+];
 
 export default function UserPage() {
-   const [recipeName, setRecipeName] = useState('');
-   const [ingredients, setIngredients] = useState('');
-   const [description, setDescription] = useState('');
-   const [cuisine, setCuisine] = useState('');
-   const [draftNotes, setDraftNotes] = useState('');
+  const [recipeName, setRecipeName] = useState("");
+  const [ingredients, setIngredients] = useState("");
+  const [description, setDescription] = useState("");
+  const [cuisine, setCuisine] = useState("");
+  const [draftNotes, setDraftNotes] = useState("");
 
+  const handlePost = (e) => {
+    e.preventDefault();
+    const recipeData = {
+      recipeName: recipeName,
+      ingredients: ingredients,
+      description: description,
+      cuisine: cuisine,
+      draftNotes: draftNotes,
+    };
 
-   //
-   const handlePost = (e) => {
-       e.preventDefault();
-       const recipeData = {
-           recipeName: recipeName,
-           ingredients: ingredients,
-           description: description,
-           cuisine: cuisine,
-           draftNotes: draftNotes,
-       };
+    axios
+      .post("http://localhost:8080/api/postedrecipe", recipeData)
+      .then((response) => {
+        console.log("data submitted successfully", response.data);
+      })
+      .catch((error) => {
+        console.error("error submitting the form", error);
+      });
 
+    resetFormFields();
+  };
 
-       axios.post('http://localhost:8080/api/postedrecipe', recipeData)
-           .then(response => {
-               console.log("data submitted successfully", response.data);
-           })
-           .catch(error => {
-               console.error('error submitting the form', error);
-           });
+  const handleDraft = (e) => {
+    e.preventDefault();
+    const recipeData = {
+      recipeName: recipeName,
+      ingredients: ingredients,
+      description: description,
+      cuisine: cuisine,
+      draftNotes: draftNotes,
+    };
 
-        setRecipeName('');
-        setIngredients('');
-        setDescription('');
-        setCuisine('');
-        setDraftNotes('');
-   };
+    axios
+      .post("http://localhost:8080/api/draft", recipeData)
+      .then((response) => {
+        console.log("data submitted successfully", response.data);
+      })
+      .catch((error) => {
+        console.error("error submitting the form", error);
+      });
 
+    resetFormFields();
+  };
 
-   const handleDraft = (e) => {
-       e.preventDefault();
-       const recipeData = {
-           recipeName: recipeName,
-           ingredients: ingredients,
-           description: description,
-           cuisine: cuisine,
-           draftNotes: draftNotes,
-       };
+  const resetFormFields = () => {
+    setRecipeName("");
+    setIngredients("");
+    setDescription("");
+    setCuisine("");
+    setDraftNotes("");
+  };
 
-
-       axios.post('http://localhost:8080/api/draft', recipeData)
-           .then(response => {
-               console.log("data submitted successfully", response.data);
-           })
-           .catch(error => {
-               console.error('error submitting the form', error);
-           });
-
-        setRecipeName('');
-        setIngredients('');
-        setDescription('');
-        setCuisine('');
-        setDraftNotes('');
-   };
-
-
-   return (
-     <Box
-       component="form"
-       sx={{
-         '& > :not(style)': { width: 500, maxWidth: '100%', },
-       }}
-       noValidate
-       autoComplete="off"
-     >
-       <h1>Upload a new recipe</h1>
-       <div>
-           <TextField
-               fullWidth
-               id="outlined-basic"
-               label="Recipe Name"
-               variant="outlined"
-               value={recipeName}
-               onChange={(e) => setRecipeName(e.target.value)}
-           />
-       </div>
-       <br></br>
-       <div>
-           <TextField
-               fullWidth
-               id="outlined-textarea"
-               label="Ingredients"
-               placeholder="Placeholder"
-               multiline
-               value={ingredients}
-               onChange={(e) => setIngredients(e.target.value)}
-           />
-       </div>
-       <br></br>
-       <div>
-           <TextField
-               fullWidth
-               id="outlined-textarea"
-               label="Directions"
-               placeholder="Placeholder"
-               multiline
-               value={description}
-               onChange={(e) => setDescription(e.target.value)}
-           />
-       </div>
-       <br></br>
-       <div>
-           <TextField
-               id="outlined-select-cuisine"
-               select
-               label="Select"
-               helperText="Please select your cuisine"
-               value={cuisine}
-               onChange={(e) => setCuisine(e.target.value)}
-               >
-               {cuisines.map((option) => (
-                   <MenuItem key={option.value} value={option.value}>
-                       {option.label}
-                   </MenuItem>
-               ))}
-           </TextField>
-       </div>
-       <br></br>
-       <div>
-           <TextField
-               fullWidth
-               id="outlined-textarea"
-               label="Draft Notes"
-               placeholder="Placeholder"
-               multiline
-               value={draftNotes}
-               onChange={(e) => setDraftNotes(e.target.value)}
-           />
-       </div>
-       <br></br>
-       <div>
-            <Button variant="contained" onClick={handlePost}>Post</Button>
-            <Button variant="contained" onClick={handleDraft}>Save as Draft</Button>
-       </div>
-     </Box>
-   );
- }
+  return (
+    <Box
+      component="form"
+      sx={{
+        "& > :not(style)": { width: 500, maxWidth: "100%" },
+      }}
+      noValidate
+      autoComplete="off"
+      className="container mt-4"
+    >
+      <h1 className="mb-4">Upload a new recipe</h1>
+      <div className="mb-3">
+        <TextField
+          fullWidth
+          id="outlined-basic"
+          label="Recipe Name"
+          variant="outlined"
+          value={recipeName}
+          onChange={(e) => setRecipeName(e.target.value)}
+        />
+      </div>
+      <br></br>
+      <div className="mb-3">
+        <TextField
+          fullWidth
+          id="outlined-textarea"
+          label="Ingredients"
+          placeholder="Placeholder"
+          multiline
+          value={ingredients}
+          onChange={(e) => setIngredients(e.target.value)}
+        />
+      </div>
+      <br></br>
+      <div className="mb-3">
+        <TextField
+          fullWidth
+          id="outlined-textarea"
+          label="Directions"
+          placeholder="Placeholder"
+          multiline
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+      </div>
+      <br></br>
+      <div className="mb-3">
+        <TextField
+          id="outlined-select-cuisine"
+          select
+          label="Select"
+          helperText="Please select your cuisine"
+          value={cuisine}
+          onChange={(e) => setCuisine(e.target.value)}
+          fullWidth
+          variant="outlined"
+        >
+          {cuisines.map((option) => (
+            <MenuItem key={option.value} value={option.value}>
+              {option.label}
+            </MenuItem>
+          ))}
+        </TextField>
+      </div>
+      <br></br>
+      <div className="mb-3">
+        <TextField
+          fullWidth
+          id="outlined-textarea"
+          label="Draft Notes"
+          placeholder="Placeholder"
+          multiline
+          value={draftNotes}
+          onChange={(e) => setDraftNotes(e.target.value)}
+        />
+      </div>
+      <br></br>
+      <div>
+        <Button
+          variant="contained"
+          onClick={handlePost}
+          className="me-2"
+          color="primary"
+        >
+          Post
+        </Button>
+        <Button
+          variant="contained"
+          onClick={handleDraft}
+          color="secondary"
+        >
+          Save as Draft
+        </Button>
+      </div>
+    </Box>
+  );
+}
