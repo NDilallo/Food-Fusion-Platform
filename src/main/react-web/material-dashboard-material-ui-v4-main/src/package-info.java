c295ac840e26eb00735a0a0405d113145be67341
mattnice <mattnice02@gmail.com>
2024-06-17 05:31:26 -0500

    added javadocs for credit

--- src/main/react-web/material-dashboard-material-ui-v4-main/src/package-info.java
+++ src/main/react-web/material-dashboard-material-ui-v4-main/src/package-info.java
@@ -4,4 +4,9 @@
  * (ie Container class, margin/spacing classes, and button variants)
  * 
  * Marisa Ban and Fritz Nastor: Search.js in views/Search folder
+ * 
+ * Matt Nice - setting which allowed switching between dark and light mode which was saved 
+ * to the database.
+ * Matt Nice extra: added mui components to Settings.js to make it look nice
+ * 
  */
