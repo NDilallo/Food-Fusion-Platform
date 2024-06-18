7f8db768141493bfe101e65ed823553b257bef9b
Dsolanki658 <166934166+Dsolanki658@users.noreply.github.com>
2024-06-18 11:09:03 -0500

    Update package-info.java

--- src/main/java/com/foodFusion/foodFusionPlatform/rdbm/homePage/package-info.java
+++ src/main/java/com/foodFusion/foodFusionPlatform/rdbm/homePage/package-info.java
@@ -9,5 +9,9 @@ package com.foodFusion.foodFusionPlatform.rdbm.homePage;
  * 
  * Matt Nice - Comment and Notification tables, CommentRepository and NotificationRepository
  * 
- * 
+ *Dhruvi Solanki- Created Chefs,Employees and Ingredients table with Chef Repository and service, Employees Repository and service
+ *Dhruvi Solanki- Ingredients Repository and services
+ *Dhruvi Solanki- Chefs provides the different specialty chefs and cuisine
+ *Dhruvi Solanki- Ingredients provides different Ingredients and types of dietary restrictions.
+ *Dhruvi Solanki- Employees provides two types of employees i.e Waiters, Chefs
  */
